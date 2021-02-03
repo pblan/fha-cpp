10e0c8f34880db19687ff10d9acbff3ac587cbca
pblan <p.blaneck@gmail.com>
2021-02-03 20:43:07 +0100

    fix

--- snippets/0x01/work_string.cpp
+++ snippets/0x01/work_string.cpp
@@ -3,64 +3,68 @@
 #include <iostream>
 using namespace std;
 
-int main() 
+int main()
 {
-    cout << endl << "--- " << __FILE__ << " ---" << endl << endl;
+     cout << endl
+          << "--- " << __FILE__ << " ---" << endl
+          << endl;
 
-	string s{"Beispiel"};                                   // (A)
+     string s{"Beispiel"}; // (A)
 
-    cout << "01|    s='" << s << "'" << endl;               // (B)
+     cout << "01|    s='" << s << "'" << endl; // (B)
 
-    cout << "-----" << endl;
+     cout << "-----" << endl;
 
-    cout << "02|    s+s='" << s+s << "' (op+)" << endl;     // (C)
-    cout << "03|    s[1]='" << s[1] << "' (op[])" << endl;  // (D)
-    s += "!";                                               // (E)
-    cout << "04|    s='" << s << "' (op+=)" << endl;
+     cout << "02|    s+s='" << s + s << "' (op+)" << endl;  // (C)
+     cout << "03|    s[1]='" << s[1] << "' (op[])" << endl; // (D)
+     s += "!";                                              // (E)
+     cout << "04|    s='" << s << "' (op+=)" << endl;
 
-    cout << "-----" << endl;
+     cout << "-----" << endl;
 
-    cout << "05|    s.size()=" << s.size() << endl;         // (F)
-    cout << "06|    s.empty()=" << s.empty() << endl;       // (G)
-    cout << "07|    s.at(1)='" << s.at(1) << "'" << endl;   // (H)
-    cout << "08|    s.substr(1,3)='"                        // (I)
-         << s.substr(1,3) << "'" << endl;
+     cout << "05|    s.size()=" << s.size() << endl;       // (F)
+     cout << "06|    s.empty()=" << s.empty() << endl;     // (G)
+     cout << "07|    s.at(1)='" << s.at(1) << "'" << endl; // (H)
+     cout << "08|    s.substr(1,3)='"                      // (I)
+          << s.substr(1, 3) << "'" << endl;
 
-    cout << "-----" << endl;
+     cout << "-----" << endl;
 
-    string::size_type pos;                                  // (J)
-    bool found;
-    
-    found = ((pos=s.find("spiel"))!=string::npos);          // (K), (L)
-    cout << "09|    find(\"spiel\") ok? " << found 
-         << ", pos=" << pos << endl;
-    found = ((pos=s.find("Reis"))!=string::npos);
-    cout << "10|    find(\"Reis\") ok? " << found << endl;
+     string::size_type pos; // (J)
+     bool found;
 
-    found = ((pos=s.rfind("i"))!=string::npos);             // (M)
-    cout << "11|    rfind(\"i\") ok? " << found 
-         << ", pos=" << pos << endl;
+     found = ((pos = s.find("spiel")) != string::npos); // (K), (L)
+     cout << "09|    find(\"spiel\") ok? " << found
+          << ", pos=" << pos << endl;
+     found = ((pos = s.find("Reis")) != string::npos);
+     cout << "10|    find(\"Reis\") ok? " << found << endl;
 
-    cout << "-----" << endl;
+     found = ((pos = s.rfind("i")) != string::npos); // (M)
+     cout << "11|    rfind(\"i\") ok? " << found
+          << ", pos=" << pos << endl;
 
-    string rep1{"Bei"};
-    pos=s.find(rep1);
-    s.replace(pos,rep1.size(),"Fussball");                  // (N)
-    cout << "12|    s.replace(...), s='" << s << "'" << endl;
+     cout << "-----" << endl;
 
-    string rep2{"!"};
-    pos=s.find(rep2);
-    s.erase(pos,rep2.size());                               // (O)
-    cout << "13|    s.erase(...), s='" << s << "'" << endl;
+     string rep1{"Bei"};
+     pos = s.find(rep1);
+     s.replace(pos, rep1.size(), "Fussball"); // (N)
+     cout << "12|    s.replace(...), s='" << s << "'" << endl;
 
-    s.insert(0,"XXL-");                                     // (P)
-    cout << "14|    insert(0,\"XXL-\"), s='" << s << "'" << endl;
+     string rep2{"!"};
+     pos = s.find(rep2);
+     s.erase(pos, rep2.size()); // (O)
+     cout << "13|    s.erase(...), s='" << s << "'" << endl;
 
-    s.append("e");                                          // (Q)
-    cout << "15|    append(\"e\"), s='" << s << "'" << endl;
+     s.insert(0, "XXL-"); // (P)
+     cout << "14|    insert(0,\"XXL-\"), s='" << s << "'" << endl;
 
-    cout << endl << "--- " << __FILE__ << " ---" << endl << endl;
-    return 0;
+     s.append("e"); // (Q)
+     cout << "15|    append(\"e\"), s='" << s << "'" << endl;
+
+     cout << endl
+          << "--- " << __FILE__ << " ---" << endl
+          << endl;
+     return 0;
 }
 
 /* Kommentierung
@@ -107,4 +111,3 @@ int main()
  * (Q)  'append' hängt Text an.
  * 
  */
- 
